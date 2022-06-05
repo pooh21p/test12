46f47a8691865623607f7546037c418b9ea6aa5f
pooh21p <uchkurov@mail.ru>
2022-06-05 10:05:30 +0300

    STEP 4

--- backend.php
+++ backend.php
@@ -1,20 +1,21 @@
-<script src = "rest.js"></script>
+<script src="rest.js"></script>
 <?php
 
-    require_once "api.php";
-    $api = new Api();
+	require_once "api.php";
+	$api = new Api();
 
-    if (isset($_REQUEST['key'])){
-        $ret = $api->rest_delete_key($_REQUEST['key']);
-    }
+	if (isset($_REQUEST['key'])){
+		$ret = $api->rest_delete_key($_REQUEST['key']);
+	}
 
-    echo '<ul id = "db_list">';
+	echo '<ul id="db_list">';
 
-    $keys = $api->redis->keys('*');
-    foreach ($keys as $key)
-    {
-        echo '<li><strong>'.$key.'</strong>: '.$api->redis->get($key).' <a href = "#" class = "remove">delete</a></li>';
-    }
-    echo '</ul>';
-    echo '<br><a href = "./backend.php">Перезагрузить страницу</a>';
-    echo '<br><a id = "reload_list" href = "#">Обновить список через REST</a>';
+	$keys = $api->redis->keys('*');
+	foreach ($keys as $key)
+	{
+		echo '<li><strong>'.$key.'</strong>: '.$api->redis->get($key).' <a href="#" class="remove">delete</a></li>';
+	}
+	
+	echo '</ul>';
+	echo '<br><a href="./backend.php">Перезагрузить страницу</a>';
+	echo '<br><a id="reload_list" href="#">Обновить список через REST</a>';
